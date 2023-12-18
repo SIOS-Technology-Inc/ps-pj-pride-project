86874760bbaa964f7630a7dac681718f428840af
田中龍之介 <ry-tanaka@sios.com>
2023-12-18 18:59:07 +0900

    fix:workflowの修正

--- src/components/modules/spring/BarChart.tsx
+++ src/components/modules/spring/BarChart.tsx
@@ -9,20 +9,20 @@ interface BarChartProps {
 export const BarChart = (props: BarChartProps) => {
   const { votesA, votesB } = props;
   // 最大票数を基準に棒グラフの高さを計算
-  const maxVotes = Math.max(votesA, votesB);
-  const heightA = (votesA / maxVotes) * 100;
-  const heightB = (votesB / maxVotes) * 100;
+  const totalVote = votesA + votesB;
+  const heightA = (votesA / totalVote) * 100;
+  const heightB = (votesB / totalVote) * 100;
 
   console.log(heightA, heightB);
 
   // React Springを使用してアニメーションを適用
-  const propsA = useSpring({ from: { height: 0 }, height: heightA });
-  const propsB = useSpring({ to: { height: heightB }, from: { height: 0 } });
+  const propsA = useSpring({ from: { height: '0%' }, height: `${heightA}%` });
+  const propsB = useSpring({ to: { height: `${heightB}%` }, from: { height: '0%' } });
 
   return (
     <>
       <div className="flex h-96 w-96 flex-row items-end justify-around">
-        <animated.div className={'h-0 w-10 bg-blue-400'} style={propsA} />
+        <animated.div className={'w-10 bg-blue-400'} style={propsA} />
         <animated.div className={'w-10 bg-red-400'} style={propsB} />
       </div>
     </>
