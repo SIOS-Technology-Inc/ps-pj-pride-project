7908bac4782e0a6bf2db19e717657949fe74773b
田中龍之介 <ry-tanaka@sios.com>
2024-03-03 12:47:10 +0900

    fix:ミタプロ削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import { TopAnimation } from './components/modules/TopAnimation';
+import { TopAnimation } from '@/components/modules/TopAnimation2';
+
 import { RouterConfig } from './router/RouterConfig';
 
 function App() {
   return (
     <>
+      {/* <TopAnimation /> */}
       <TopAnimation />
       <BrowserRouter>
         <RouterConfig />
