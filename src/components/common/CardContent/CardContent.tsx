11158550b75ce4b11dfeeed0f120380257064607
atomic-kanta-sasaki <kanta.sasaki.0111@gmail.com>
2024-07-01 13:54:57 +0900

    自慢の詳細分にURIが含まれている場合にはリンクにする

--- src/components/common/CardContent/CardContent.tsx
+++ src/components/common/CardContent/CardContent.tsx
@@ -1,3 +1,4 @@
+import Linkify from "linkify-react";
 type CardContentProps = {
   content: string;
   label?: string;
@@ -6,12 +7,16 @@ type CardContentProps = {
 
 export const CardContent = (props: CardContentProps) => {
   const { content, label, horizontal } = props;
+  const options = {
+    className: 'break-all text-black',
+    tagName: 'li',
+  }
   if (horizontal) {
     return (
       <>
         <ul className="flex grow flex-row justify-between text-lg">
           {label && <li className="text-gray">{label}</li>}{' '}
-          <li className="break-all text-black">{content}</li>
+          <Linkify options={options}>{content}</Linkify>
         </ul>
       </>
     );
